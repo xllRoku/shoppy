44ffb2d0c2baf7d2536e2d309345fc65e558f5c4
xllRoku <jhinlmlz@gmail.com>
2022-11-16 12:42:46 -0500

    first commit

---
+++ src/cosntans/items.ts
@@ -0,0 +1,88 @@
+export const FRUITS = [
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/5428/5428275.png",
+    title: "Avocado",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/3151/3151771.png",
+    title: "Banana",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/2952/2952386.png",
+    title: "Bunch of carrots 5pcs",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/4868/4868143.png",
+    title: "Chicken 1kg",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/1357/1357806.png",
+    title: "Pre-cooked con 450g",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/4825/4825525.png",
+    title: "Mandarin Nadorcott",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/7541/7541584.png",
+    title: "Pielle de sapo Melon",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/5052/5052771.png",
+    title: "Watermelon",
+  },
+];
+
+export const MEAT_AND_FISTH = [
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/7657/7657778.png",
+    title: "Chicken leg box",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/1895/1895685.png",
+    title: "Chicken 1kg",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/6598/6598169.png",
+    title: "Pork fillets 450g",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/3162/3162144.png",
+    title: "Salmon 1kg",
+  },
+];
+
+export const BERAGES = [
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/5428/5428275.png",
+    title: "Avocado",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/3151/3151771.png",
+    title: "Banana",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/2952/2952386.png",
+    title: "Bunch of carrots 5pcs",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/4868/4868143.png",
+    title: "Chicken 1kg",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/1357/1357806.png",
+    title: "Pre-cooked corn 450g",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/4825/4825525.png",
+    title: "Mandarin Nadorcott",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/7541/7541584.png",
+    title: "Pielle de sapo Melon",
+  },
+  {
+    imageUrl: "https://cdn-icons-png.flaticon.com/128/5052/5052771.png",
+    title: "Watermelon",
+  },
+];
